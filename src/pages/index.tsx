901731f61e932390bdc7c1618bc6baba96478b1a
Eduard Shavyrko <edshav@protonmail.com>
2021-03-18 23:05:06 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+      </Head>
+
+      <div>HomePage</div>
+    </>
+  );
+};
+
+export default HomePage;
