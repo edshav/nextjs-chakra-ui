deafdf31c869ac135cbe8772ef17d703af3c4316
Eduard <edshav@protonmail.com>
2021-03-28 13:53:07 +0300

    add chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import { NextPage } from "next";
-import Head from "next/head";
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { Button, CircularProgress, Menu, MenuButton, MenuItem, MenuList, Textarea } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 const HomePage: NextPage = () => {
   return (
@@ -8,7 +10,24 @@ const HomePage: NextPage = () => {
         <title>My App</title>
       </Head>
 
-      <div>HomePage</div>
+      <div>
+        HomePage
+        <Textarea rows={1} />
+        <CircularProgress isIndeterminate />
+        <Menu closeOnBlur >
+          {({ isOpen }) => (
+            <>
+              <MenuButton isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />}>
+                {isOpen ? 'Close' : 'Open'}
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem onClick={() => alert('Kagebunshin')}>Create a Copy</MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      </div>
     </>
   );
 };
